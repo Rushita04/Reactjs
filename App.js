f476428a50c197a07d386fa5f31bac0a4bf6bee9
Rushita04 <137377512+Rushita04@users.noreply.github.com>
2024-02-09 09:56:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+//import logo from './logo.svg';
+import './App.css';
+// import EventHandlingInClass from './components/EventHandlingInClass';
+// import EventHandlingInFunction from './components/EventHandlingInFunction';
+// import Info from './components/Info';
+// import Replay from './components/Replay';
+// import compWithoutJSx from './components/compWithoutJSx';
+//  import PropsInFunComp from './components/PropsInFunComp';
+// import PropsTypeChecKing from './components/PropsTypeChecKing';
+// import Info from './components/Info';
+// import { HelloUser } from './components/HelloUser';
+// import Counter from './components/Counter';
+// import EventBinding from './components/EventBinding';
+
+//  import Greeting from './components/Greeting';
+// import Username from './components/Username';
+// import Greetings from './components/Greetings';
+// import Myinfo from './components/Myinfo';
+// import Userinfo from './components/Userinfo';
+// import Info1 from './components/Info1';
+// import DestructureClassProps from './components/DestructureClassProps';
+// import EventHandlingInFunction from './components/EventHandlingInFunction';
+// import ConditionalRendering from './ConditionalRendering';
+// import MyName from './components/MyName';
+import ControllComponent from './components/ControllComponent';
+
+
+
+function App() {
+
+  return (
+   <div className="App">
+    {/* <Myinfo nm="Rushita"/> */}
+    
+   {/* <Greeting/>
+   <Username/> */}
+   {/* <Greetings/> */}
+   {/* <Myinfo/> */}
+   {/* <Userinfo  username="Rushita" place="Pune"/>
+   <Userinfo username="Sakshi"/> */}
+   {/* <Info name="Rushita" ntv="Pune" edu="BCA"/> */}
+   {/* <DestructureClassProps name="Rushita" ntv="pune" /> */}
+   {/* <EventHandlingInFunction/> */}
+   {/* <EventHandlingInClass/> */}
+   {/* <ConditionalRendering/> */}
+  {/* //  <MyName/> */}
+  <ControllComponent/>
+
+
+  
+
+   </div>
+  );
+}
+
+export default App;
+
